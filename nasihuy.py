4fce085aca820af76cf310076244e25ecb3f641e
22_Sandy Pratama <sandy.tkjsmeksa25@gmail.com>
2026-08-10 20:28:02 +0700

    Add files via upload

--- nasihuy.py
+++ nasihuy.py
@@ -1,5 +1,12 @@
-angka = int(input("Masukkan angka: "))
-if angka % 2 == 0:
-    print("Bilangan Genap")
-else:
-    print("Bilangan Ganjil")
+for i in range(100):
+    angka = int(input("Masukkan angka: "))
+
+    if angka % 2 == 0:
+        print("Genap")
+    else:
+        print("Ganjil")
+
+    lanjut = input("Masukkan lagi? (y/n): ")
+
+    if lanjut == "n":
+        break
