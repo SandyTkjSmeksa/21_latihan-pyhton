2794df33c67a15d25250d0e5b835fe946327492b
22_Sandy Pratama <sandy.tkjsmeksa25@gmail.com>
2026-09-01 20:55:29 +0700

    kalkulator_bangun.py

---
+++ kalkulator_bangun.py
@@ -0,0 +1,11 @@
+def hitung_luas_bujursangkar(sisi):
+    return sisi * sisi
+
+def hitung_keliling_bujursangkar(sisi):
+    return 4 * sisi
+
+def luas_lingkaran_custom(r):
+    return 3.14 * r * r
+
+def luas_segitiga_custom(alas, tinggi):
+    return 0.5 * alas * tinggi
